1282bd053f1a7c1e2b02adb69a67dd58aedcd0e6
bekwdev <bekwdev@gmail.com>
2020-02-21 15:22:54 -0500

    Added some styling to SignUpScreen

--- src/SignUpScreen.js
+++ src/SignUpScreen.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import {View, StyleSheet} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import { Input, Button , Text} from 'react-native-elements';
-
+import {Input, Button, Text} from 'react-native-elements';
+import Spacer from './components/Spacer';
 
 const SignUpScreen = ({navigation}) => {
   return (
-    <>
-     <Text h3>Sign Up for Application</Text>
-     <Input  label = "Email"/>
-     <Input label='Password' />
-          <Button
-           title="Sign Up" type="outline" raised />
-    </>
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>Sign Up for Application</Text>
+      </Spacer>
+      <Input label="Email" />
+      <Spacer />
+      <Input label="Password" />
+      <Spacer>
+        <Button title="Sign Up" type="outline" raised />
+      </Spacer>
+    </View>
   );
 };
+SignUpScreen.navigationOptions = () => {
+  return {
+    // header : null     // old way , it will be removed in new versions soon
+    headerShown: false, //  changed doe to update of react native
+  };
+}; //  bu funksiya teppadegei header ni uchiradi    , buni funksiyaga bog`lamasdan objekt qilsayam buladi
 
-const styles = StyleSheet.create ({});
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1, //  chegarani iloji boricha kottalashtiradi
+    justifyContent: 'center', //  chegara kottalashganidan kegin ichidage elementlarni  o`rtaga joylashtiradi
+    marginBottom: 200,
+  },
+});
 
 export default SignUpScreen;
