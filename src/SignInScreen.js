f8011f2a2279c0b932a3fad3e3e3a99c030fd00a
bekwdev <bekwdev@gmail.com>
2020-03-06 14:03:35 -0500

    edited SignIn screen

--- src/SignInScreen.js
+++ src/SignInScreen.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import AuthForm from './components/AuthForm'
+import NavLink from './components/NavLink'
 
 const SignInScreen = () => {
-  return <Text style={{fontSize: 48}}>SignInScreen</Text>;
-};
+  return (
+    <View style={styles.container}>
+      <AuthForm
+        headerText='Sign Up for Your Account'
+        errorMessage=''
+        onSubmit={() => { }}
+        submitButtonText='Sign In'
+      />
+      <NavLink
+        text='Don`t have an account? sign up'
+        routeName='SignUp'
+      />
 
-const styles = StyleSheet.create ({});
+
+
+    </View>
+  )
+}
+SignInScreen.navigationOptions = () => { headerShown: false };
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, //  chegarani iloji boricha kottalashtiradi
+    justifyContent: 'center', //  chegara kottalashganidan kegin ichidage elementlarni  o`rtaga joylashtiradi
+    marginBottom: 200, // without margin keyboard will close signup button while typing procces on input
+  },
+})
+
 
 export default SignInScreen;
