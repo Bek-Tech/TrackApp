dd3f44d804f333c4441f51a3812fd084b63d4e72
bekwdev <bekwdev@gmail.com>
2020-03-10 16:28:52 -0300

    fixed bug with catching error when user denied access to location services , used expo permission istead of expo location

--- src/TrackCreateScreen.js
+++ src/TrackCreateScreen.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
+import { requestPermissionsAsync } from 'expo-location'
+import * as Permissions from 'expo-permissions';
 import { SafeAreaView } from 'react-navigation' // sets auto msrgin on sides regarding  size  of display and  gadgets model 
-import Maps from './components/Map'
+import Maps from './components/Maps'
+
+
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    const location = await Permissions.askAsync(Permissions.LOCATION)
+    if (location.status !== 'granted') {
+      setErr('error')
+    }
+  }
+
+
+  useEffect(() => {
+    startWatching()
+  }, [])
+
+
   return <SafeAreaView forceInset={{ top: 'always' }}>
     <Text h2>TrackCreateScreen</Text>
     <Maps />
+    {err ? <Text>Do not have a permission for access location services</Text> : null}
+
   </SafeAreaView>
 
 };
